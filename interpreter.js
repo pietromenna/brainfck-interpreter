0e53869e6368afeae380063685c68c4bb17def06
Pietro F. Menna <pietromenna@yahoo.com>
2015-06-03 20:59:08 -0400

    First refactor

--- interpreter.js
+++ interpreter.js
@@ -1,19 +1,19 @@
-var VirtualMachine = {
-	memory: new ArrayBuffer(30000),
-	instructions: [],
-	instruction_pointer: 0,
-	data_pointer: 0,
-	output: "",
-	initialize_machine: function(){
-		for (var i;i<this.memory.length;i++) memory[i] = 0;
+var VirtualMachine = function() {
+	var memory = new ArrayBuffer(30000);
+	var instructions = [];
+	var instruction_pointer = 0;
+	var data_pointer = 0;
+	var output =  "";
+	this.initialize_machine = function(){
+		for (var i;i<memory.length;i++) memory[i] = 0;
 		instruction_pointer = 0;
 		data_pointer = 0;
-	},
-	load_program: function(source_code) {
+	}
+	this.load_program = function(source_code) {
 		instructions = source_code;
 		this.initialize_machine();
-	},
-	run: function(){
+	}
+	this.run = function(){
 		while(instruction_pointer<instructions.length){
 			switch(instructions[instruction_pointer]) {
 				case '>':
@@ -33,6 +33,7 @@ var VirtualMachine = {
 					break;
 				case ',':
 					//accept one byte of input, storing its value in the byte at the data pointer.
+					//PFM: I haven`t think yet on how to implement this! (UI sense and usability)
 					break;
 				case '[':
 					//if the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
@@ -43,21 +44,20 @@ var VirtualMachine = {
 			}
 			instruction_pointer = instruction_pointer + 1;
 		}
-	},
-	get_output: function() {
+	}
+	this.get_output = function() {
 		return output;
 	}
 };
 
 var process_input = function(source_code) {
 	var machine = new VirtualMachine();
-	machine.load_program();
+	machine.load_program(source_code);
 	machine.run();
 	return machine.get_output();
 }
 
 var process_file = function(){
-	alert((document.getElementById("input").value)[0]);
 	var input_source = document.getElementById("input").value;
 	var output = process_input(input_source);
 	document.getElementById("output").innerHTML = output;
