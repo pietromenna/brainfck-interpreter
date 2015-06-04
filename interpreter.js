5d67900a81df94933c574ed1250e6436d80a40c2
Pietro F. Menna <pietromenna@yahoo.com>
2015-06-04 18:13:57 -0400

    Seems to be working

--- interpreter.js
+++ interpreter.js
@@ -1,17 +1,48 @@
 var VirtualMachine = function() {
-	var memory = new ArrayBuffer(30000);
+	var memory = new Array(300);
 	var instructions = [];
 	var instruction_pointer = 0;
 	var data_pointer = 0;
 	var output =  "";
 	this.initialize_machine = function(){
-		for (var i;i<memory.length;i++) memory[i] = 0;
+		for (var i=0;i<memory.length;i++) memory[i] = 0;
 		instruction_pointer = 0;
 		data_pointer = 0;
 	}
 	this.load_program = function(source_code) {
 		instructions = source_code;
 		this.initialize_machine();
+	}
+	this.findMatching = function(instruction){
+		var nextInstruction = instruction;
+		var direction;
+		count = 1;
+		if (instructions[instruction] === ']'){
+			nextInstruction = nextInstruction - 1;
+			direction = 'up';
+		} else {
+			nextInstruction = nextInstruction + 1;
+			direction = "down";
+		}
+		while(count != 0){
+			if ((instructions[nextInstruction] == ']' && direction == 'up') ||
+				(instructions[nextInstruction] == '[' && direction == 'down')) {
+				count = count + 1;
+			} else if ((instructions[nextInstruction] == ']' && direction == 'down') ||
+				(instructions[nextInstruction] == '[' && direction == 'up')) {
+				count = count - 1;
+			}
+			if (count == 0) {
+				nextInstruction = nextInstruction + 1;
+				continue;
+			}
+			if (direction == 'up') {
+				nextInstruction = nextInstruction - 1;
+			} else {
+				nextInstruction = nextInstruction + 1;
+			}
+		}
+		return nextInstruction;
 	}
 	this.run = function(){
 		while(instruction_pointer<instructions.length){
@@ -29,7 +60,7 @@ var VirtualMachine = function() {
 					memory[data_pointer] = memory[data_pointer] - 1;
 					break;
 				case '.':
-					output = output + memory[data_pointer];
+					output = output + String.fromCharCode(memory[data_pointer]);
 					break;
 				case ',':
 					//accept one byte of input, storing its value in the byte at the data pointer.
@@ -37,9 +68,17 @@ var VirtualMachine = function() {
 					break;
 				case '[':
 					//if the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
+					if (memory[data_pointer] == 0) {
+						instruction_pointer = this.findMatching(instruction_pointer);
+						continue;
+					}
 					break;
 				case ']':
 					//if the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.
+					if (memory[data_pointer] != 0){
+						instruction_pointer = this.findMatching(instruction_pointer);
+						continue;
+					}
 					break;
 			}
 			instruction_pointer = instruction_pointer + 1;
